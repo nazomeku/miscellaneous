a124c4093bb0d261fcb979caa26f83bf7402e3be
nazomeku <cols.nazo@gmail.com>
2017-08-31 14:59:38 +0200

    add imgrab script

---
+++ imgrab.py
@@ -0,0 +1,18 @@
+import time
+import numpy as np
+from PIL import ImageGrab
+import cv2
+
+
+def screen_record():
+    """Capture screen region and show it in new window."""
+    last_time = time.time()
+    while True:
+        #800x600 windowed mode
+        printscreen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        print('loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
